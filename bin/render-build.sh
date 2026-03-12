5ed9ee2b5ff5e72867568bf65a6554e4f9eb7d87
kkimura <kazuto.kimura@g-seed.co.jp>
2026-03-12 12:51:01 +0900

    Renderデプロイ設定を追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
-bundle exec rails db:migrate:cache
-bundle exec rails db:migrate:queue
-bundle exec rails db:migrate:cable
+bundle exec rails db:prepare
